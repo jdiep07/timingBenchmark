ffd2654c1c49f3c7be99804a3203e1d14e1c24ee
Jimmy Diep <jdiep07@gmail.com>
2023-08-29 16:04:53 -0700

    phase 1 done

---
+++ phase-1.js
@@ -0,0 +1,23 @@
+// Adds up positive integers from 1-n
+function addNums(n) {
+  // Fill this in
+  let sum = 0
+
+  for (let i = 0; i <= n; i++) {
+    sum += i
+  }
+  return sum
+}
+
+// Adds up values of addNums(1) through addNums(n)
+function addManyNums(n) {
+  // Fill this in
+  let total = 0
+
+  for (let i = 1; i <= n; i++) {
+    total += addNums(i)
+  }
+  return total
+}
+
+module.exports = [addNums, addManyNums]
