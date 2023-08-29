393509bd26d339fcb19fcbdcae362be9494dca6c
LoganLaRose <laroselogan@gmail.com>
2023-08-29 19:10:59 -0400

    phase 2

--- phase-2.js
+++ phase-2.js
@@ -2,14 +2,20 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
-
+  const results = [];
+  for (let i = increment; i <= 10 * increment; i += increment) {
+    results.push(addNums(i));
+  }
+  return results;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
-
+  const results = [];
+  for (let i = increment; i <= 10 * increment; i += increment) {
+    results.push(addManyNums(i));
+  }
+  return results;
 }
 
 module.exports = [addNums10, addManyNums10];
